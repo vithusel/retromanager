41ec7f0c171f216b2b1c8c537d9a6ecf5468122f
vithusel <contact@vithusel.me>
2023-01-07 17:56:58 +0000

    Add Git function and start repo list

--- functions.sh
+++ functions.sh
@@ -12,6 +12,9 @@ HEIGHT=0
 WIDTH=0
 pendingupdates=`apt-get -q -y --ignore-hold --allow-change-held-packages --allow-unauthenticated -s dist-upgrade | /bin/grep  ^Inst | wc -l`
 
+#Repo Variables
+mainrepo=https://git.vithusel.me/vithusel/retromanager.git
+
 # FUNCTIONS
 
 display_result() {
@@ -87,4 +90,8 @@ if ! dpkg-query -W -f='${Status}' "${1}" | grep -q "ok installed"
 then
     apt-get update -q4 & apt-get install "${1}" -y
 fi
+}
+
+clone_repo() {
+    git clone
 }
