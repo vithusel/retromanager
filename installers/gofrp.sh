6fcae5cec88b8bc21e1c4bed333da6ced4cb077a
Vit Selvarajah <vit@vithuselservices.co.uk>
2023-04-01 14:27:13 +0100

    Update gofrp.sh

--- installers/gofrp.sh
+++ installers/gofrp.sh
@@ -2,9 +2,7 @@
 
 # Retro Manager © - 2023, https://vithuselservices.co.uk
 
-    eval "$(curl -sfLS https://import.sh)"
-
-    import "https://git.vithuselservices.co.uk/vithusel/retromanager/-/raw/main/functions.sh"
+    source <(curl -s https://git.vithuselservices.co.uk/vithusel/retromanager/-/raw/main/functions.sh)
 
     application=GoFRP
     consent=$(echo Do you have a AMD64 Machine?)
